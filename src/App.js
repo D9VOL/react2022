4f2a06a60b9045fa3490bd36688b53988da9bbd3
D9VOL <gladun.an@gmail.com>
2022-09-18 19:03:57 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Simpsons from "./components/Simpsons";
 
 function App() {
   return (
     <div>
+      <h2> Simpsons Famili</h2>
+
+        <Simpsons/>
 
     </div>
   );
